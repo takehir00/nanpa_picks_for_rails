59b75028d06e572f1265ec950d2f473910bdf296
takehir00 <0kutakehiro15@gmail.com>
2019-04-14 16:39:24 +0900

    migrate usersテーブル

--- db/migrate/20190414072526_create_users.rb
+++ db/migrate/20190414072526_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :image_name
-      t.string :password
+      t.string :password, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_074303) do
+ActiveRecord::Schema.define(version: 2019_04_14_072526) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "url", null: false
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2019_04_13_074303) do
     t.datetime "updated_at", null: false
     t.text "title"
     t.text "src"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image_name"
+    t.string "password", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
