d4abb8fdd8caf76821ef7a2f0b9421202f201da2
takehir00 <0kutakehiro15@gmail.com>
2019-04-14 16:35:57 +0900

    adminルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  #admin機能
+  get 'admin/top' => 'admin#top'
+  get 'admin/articles/index' => 'admin#articles_index'
+  get 'admin/articles/new' => 'admin#articles_new'
+  get 'admin/articles/edit/:id' => 'admin#articles_edit'
 
+  #articles機能
   root to: 'articles#top'
   resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
