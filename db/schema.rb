731bb319945bf17775c2007a079d5e09fb3d0a9f
takehir00 <0kutakehiro15@gmail.com>
2019-04-14 16:35:20 +0900

    generateユーザーモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_231709) do
+ActiveRecord::Schema.define(version: 2019_04_13_074303) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "url", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "title"
+    t.text "src"
   end
 
 end
