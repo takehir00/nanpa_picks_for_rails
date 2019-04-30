a8fdbfdfa53901be3893c7dac099694c6e2a5a71
takehir00 <0kutakehiro15@gmail.com>
2019-04-30 11:10:32 +0900

    記事詳細画面にコメント表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,16 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    puts @article.id
     @comment = Comment.new
+    #街灯のarticleのidど合致するarticle_idをもつcommentを表示する
+    comments = Comment.where(article_id: @article.id)
+    #と思ったけど今ってユーザーidしか持ってないからview側でユーザーが表示できない。このメソッド内で@commentsをぶん回してユーザー情報も持ったオブジェクトを生成するとかどうだろ。DTO的な。
+    comments.each do |comment|
+      comment.user = User.find_by(id: comment.user_id)
+    end
+    #なんかごちゃついてる。commentモデルクラスでwhereの検索とユーザーの検索やってくれるメソッド定義して、ここではそれを呼び出すだけな感じがよさそう。@comments = Comment.find_by_article_idみたいな。とりあえず動かしてみる
+    @comments = comments
   end
 
   def create
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
 validates :content, presence: true
+
+attr_accessor :user
 end
